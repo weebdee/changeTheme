d2c8d21ad6e90320b73ecc2fa6ad9ab9723b3cea
Amirden-st <82529791+Amirden-st@users.noreply.github.com>
2021-12-21 00:17:55 +0600

    Update script.js

--- hw_Diana_09-02_THEMEMODE/script.js
+++ hw_Diana_09-02_THEMEMODE/script.js
@@ -3,28 +3,35 @@ var btnDark = document.getElementById('darkMode')
 var btnPastel = document.getElementById('pastelMode')
 var btnFunky = document.getElementById('funkyMode')
 var link = document.getElementById("Link");
+// хорошая идея!
 var currTheme = link.getAttribute("href");
 var theme = "";
 
+
+// можно просто:
+// btnLight.addEventListener("click", LightMode);
 btnLight.addEventListener("click", function () { LightMode(); });
 
-function LightMode() {
+function LightMode() { 
     let lightTheme = "styles/light.css";
-
+    // вы пытаетесь оптимизировать ваш код - это хорошо,
+    // но здесь это условее скорее лишнее, т.к. операция не такая дорогостоящая
     if(currTheme !== lightTheme)
     {
    	 currTheme = lightTheme;
    	 theme = "light";
     }
-
+    
+    // Можно прописать эту строку внутри условия сверху, раз уж мы хотим всё оптимизировать)
     link.setAttribute("href", currTheme);
 }
+// Ну и снизу тоже самое
 
 btnDark.addEventListener("click", function () { DarkMode(); });
 
 function DarkMode() {
     let darkTheme = "styles/dark.css";
-
+    
     if(currTheme !== darkTheme)
     {
    	 currTheme = darkTheme;
@@ -61,3 +68,7 @@ function FunkyMode() {
 
     link.setAttribute("href", currTheme);
 }
+
+
+// Очень креативненько)
+
